72d986bce6a883189c4ee62a09cfc1e594fe77f4
RMACARI <ruslan.macari@orange.md>
2020-10-28 18:10:41 +0200

    lesson 16 Services and DI

--- src/app/products.component.ts
+++ src/app/products.component.ts
@@ -1,18 +1,20 @@
-import {Component} from '@angular/core';
-import {ProductComponent} from './product/product.component';
-import {AbstractControl, Form} from '@angular/forms';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {AbstractControl} from '@angular/forms';
+import {ProductsService} from './products.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html'
 })
 
-export class ProductsComponent {
+export class ProductsComponent implements OnInit, OnDestroy{
   productName = 'A Book';
   isDisabled = true;
-  products = ['A Book', 'A Tree'];
+  products = [];
+  private productsSubscription: Subscription;
 
-  constructor() {
+  constructor(private productsService: ProductsService) {
     setTimeout(() => {
       this.isDisabled = false;
     }, 3000);
@@ -20,11 +22,27 @@ export class ProductsComponent {
 
   onAddProduct(form: AbstractControl): void {
     if (form.valid) {
-      this.products.push(form.value.productName);
+      // this.products.push(form.value.productName);
+      this.productsService.addProduct(form.value.productName);
     }
   }
 
   onRemoveProduct(productName: string): void {
     this.products = this.products.filter(p => p !== productName);
+  }
+
+  ngOnInit(): void {
+    this.products = this.productsService.getProducts();
+    this.productsSubscription = this.productsService.productsUpdated.subscribe(() => {
+      this.products = this.productsService.getProducts();
+    });
+  }
+
+  getProducts(): string[] {
+    return this.productsService.getProducts();
+  }
+
+  ngOnDestroy(): void {
+    this.productsSubscription.unsubscribe();
   }
 }
---
+++ src/app/products.service.ts
@@ -0,0 +1,22 @@
+import {fromArray} from 'rxjs/internal/observable/fromArray';
+import {Subject} from 'rxjs';
+
+export class ProductsService {
+  private products: string[] = ['A Book'];
+  productsUpdated = new Subject();
+
+  addProduct(product: string): void {
+    this.products.push(product);
+    this.productsUpdated.next();
+  }
+
+  getProducts(): string[] {
+    return Object.assign([], this.products);
+    // return [...this.products];
+  }
+
+  deleteProduct(product: string): void {
+    this.products = this.products.filter(p => p !== product);
+    this.productsUpdated.next();
+  }
+}
