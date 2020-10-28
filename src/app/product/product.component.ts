61a634fd35abbbcde080c679961931d887598219
ruslanMacari <ruslan.macari@orange.md>
2020-10-28 18:15:31 +0200

    added product component

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import {ProductsService} from '../products.service';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  @Input()productName: string;
+  @Output()productClicked = new EventEmitter();
+  constructor(private productService: ProductsService) { }
+
+  ngOnInit(): void {
+  }
+
+  onClicked(): void {
+    this.productService.deleteProduct(this.productName);
+  }
+
+}
